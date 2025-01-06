351a0911cbca7157394df22bef232eaa127ce5c8
Tobias Wolf <tobias.wolf@nitrado.net>
2025-01-06 23:18:09 +0100

    Fix the failing test

--- internal/component/common/loki/client/client_test.go
+++ internal/component/common/loki/client/client_test.go
@@ -406,7 +406,7 @@ func TestClient_Handle(t *testing.T) {
 			expectedMetrics: `
                               # HELP loki_write_sent_entries_total Number of log entries sent to the ingester.
                               # TYPE loki_write_sent_entries_total counter
-                              loki_write_sent_entries_total{host="__HOST__",tenant=""} 2.0
+                              loki_write_sent_entries_total{host="__HOST__",tenant="tenant-default"} 2.0
                               # HELP loki_write_dropped_entries_total Number of log entries dropped because failed to be sent to the ingester after all retries.
                               # TYPE loki_write_dropped_entries_total counter
                               loki_write_dropped_entries_total{host="__HOST__", reason="ingester_error", tenant="tenant-default"} 0
